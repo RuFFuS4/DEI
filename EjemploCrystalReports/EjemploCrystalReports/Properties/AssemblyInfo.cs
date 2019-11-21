5b29693f3584a8e68b6830d4808f11a22e24cad0
RuFFuS4 <rgr93.4@outlook.es>
2019-11-21 11:57:49 +0100

    Primer CrystalReport Creado

---
+++ EjemploCrystalReports/EjemploCrystalReports/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EjemploCrystalReports")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EjemploCrystalReports")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9d644e55-3c58-4d8e-a582-e4478dca6cd6")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
